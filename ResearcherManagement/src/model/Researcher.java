ca47a459eaaa36a4f88279ff95bc509287606111
Priyanka-Kugapriya <shwara99@gmail.com>
2021-04-20 23:35:46 +0530

    changes done on researcher.java model class under readUsers

--- ResearcherManagement/src/model/Researcher.java
+++ ResearcherManagement/src/model/Researcher.java
@@ -22,7 +22,7 @@ public class Researcher {
 			 return con; 
 	 } 
 	
-	public String insertItem(String r_name, String r_surname, String r_email, String r_password, String r_productType, String r_registrationDate) 
+	public String insertUser(String r_name, String r_surname, String r_email, String r_password, String r_productType, String r_registrationDate) 
 	 { 
 			 String output = ""; 
 			 try
@@ -62,64 +62,70 @@ public class Researcher {
 			 return output; 
 	 } 
 	
-//	 public String readItems() 
-//	 { 
-//		 
-//			 String output = ""; 
-//			 
-//			 try
-//			 { 
-//				 
-//				 	Connection con = connect(); 
-//				 	
-//					if (con == null) 
-//					{return "Error while connecting to the database for reading."; } 
-//	 
-//					// Prepare the html table to be displayed
-//					output = "<table border='1'><tr><th>Item Code</th><th>Item Name</th>" +
-//							 "<th>Item Price</th>" + 
-//							 "<th>Item Description</th>" +
-//							 "<th>Update</th><th>Remove</th></tr>"; 
-//	 
-//					 String query = "select * from items"; 
-//					 Statement stmt = con.createStatement(); 
-//					 ResultSet rs = stmt.executeQuery(query); 
-//					 
-//					 // iterate through the rows in the result set
-//					 while (rs.next()) 
-//					 { 
-//							 String itemID = Integer.toString(rs.getInt("itemID")); 
-//							 String itemCode = rs.getString("itemCode"); 
-//							 String itemName = rs.getString("itemName"); 
-//							 String itemPrice = Double.toString(rs.getDouble("itemPrice")); 
-//							 String itemDesc = rs.getString("itemDesc"); 
-//							 
-//							 // Add into the html table
-//							 output += "<tr><td>" + itemCode + "</td>"; 
-//							 output += "<td>" + itemName + "</td>"; 
-//							 output += "<td>" + itemPrice + "</td>"; 
-//							 output += "<td>" + itemDesc + "</td>"; 
-//							 
-//							 // buttons
-//							 output += "<td><input name='btnUpdate' type='button' value='Update' class='btn btn-secondary'></td>"
-//							 + "<td><form method='post' action='items.jsp'>"+ "<input name='btnRemove' type='submit' value='Remove'class='btn btn-danger'>"
-//							 + "<input name='itemID' type='hidden' value='" + itemID 
-//							 + "'>" + "</form></td></tr>"; 
-//							 
-//					 } 
-//					 con.close(); 
-//					 
-//					 // Complete the html table
-//					 output += "</table>"; 
-//					 
-//			} 
-//			 catch (Exception e) 
-//			 { 
-//					 output = "Error while reading the items."; 
-//					 System.err.println(e.getMessage()); 
-//			 } 
-//			 return output; 
-//	 }
+	 public String readUsers() 
+	 { 
+		 
+			 String output = ""; 
+			 
+			 try
+			 { 
+				 
+				 	Connection con = connect(); 
+				 	
+					if (con == null) 
+					{return "Error while connecting to the database for reading."; } 
+	 
+					// Prepare the html table to be displayed
+					output = "<table border='1'><tr><th>User name</th><th>Users Surname</th>" +
+							 "<th>E-mail address</th>" + 
+							 "<th>Password</th>" +
+							 "<th>Product Type</th>" +
+							 "<th>Registration Date</th>" +
+							 "<th>Update</th><th>Remove</th></tr>"; 
+	 
+					 String query = "select * from items"; 
+					 Statement stmt = con.createStatement(); 
+					 ResultSet rs = stmt.executeQuery(query); 
+					 
+					 // iterate through the rows in the result set
+					 while (rs.next()) 
+					 { 
+							 String userID = Integer.toString(rs.getInt("userID")); 
+							 String name = rs.getString("name"); 
+							 String surname = rs.getString("surname"); 
+							 String email = rs.getString("email"); 
+							 String password = rs.getString("password"); 
+							 String productType = rs.getString("productType");  
+							 String registrationDate = rs.getString("registrationDate"); 
+							 
+							 // Add into the html table
+							 output += "<tr><td>" + name + "</td>"; 
+							 output += "<td>" + surname + "</td>"; 
+							 output += "<td>" + email + "</td>"; 
+							 output += "<td>" + password + "</td>"; 
+							 output += "<td>" + productType + "</td>"; 
+							 output += "<td>" + registrationDate + "</td>"; 
+							 
+							 // buttons
+							 output += "<td><input name='btnUpdate' type='button' value='Update' class='btn btn-secondary'></td>"
+							 + "<td><form method='post' action='items.jsp'>"+ "<input name='btnRemove' type='submit' value='Remove'class='btn btn-danger'>"
+							 + "<input name='itemID' type='hidden' value='" + userID 
+							 + "'>" + "</form></td></tr>"; 
+							 
+					 } 
+					 con.close(); 
+					 
+					 // Complete the html table
+					 output += "</table>"; 
+					 
+			} 
+			 catch (Exception e) 
+			 { 
+					 output = "Error while reading users"; 
+					 System.err.println(e.getMessage()); 
+			 } 
+			 return output; 
+	 }
 //	 
 //	 public String updateItem(String ID, String code, String name, String price, String desc)
 //	 { 
